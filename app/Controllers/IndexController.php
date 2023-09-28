77ff0e04a6caa0237d00d9aad792ce0d76b463e0
prospero <g0lden_vo1d@protonmail.com>
2023-09-28 14:55:02 +0200

    sync

--- app/Controllers/IndexController.php
+++ app/Controllers/IndexController.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Controllers;
 
 use App\View;
+use GuzzleHttp\Client;
 
 class IndexController
 {
@@ -17,6 +18,24 @@ class IndexController
 
     public function index()
     {
+        $client = new Client([
+            'base_uri' => "https://dummyjson.com/"
+        ]);
+
+        $response = $client->patch("/products/1", [
+            "headers" => [
+                "Content-type" => "application/json; charset=UTF-8"
+            ],
+            "body" => json_encode([
+                "title" => "aaaaaaaaaaaaaaaaa"
+            ])
+        ]);
+
+        $responseData = json_decode($response->getBody()->getContents());
+
+        dd($responseData);
+        die();
+
         return $this->view->render("index");
     }
 }
